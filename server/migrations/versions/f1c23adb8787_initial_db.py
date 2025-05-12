b686c403d9ed22dfae0b0be536c3ff3aecae1f8c
homeira kalim <homeirakalim@gmail.com>
2025-05-12 15:37:35 -0400

    initial commit

---
+++ server/migrations/versions/f1c23adb8787_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: f1c23adb8787
+Revises: 
+Create Date: 2025-05-12 15:37:16.359783
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f1c23adb8787'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
